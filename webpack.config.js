b6265df97a5cc7a3537d835f8115a3240f0d1907
Stefan Kruijt <stefankruijt1991@gmail.com>
2017-06-11 14:58:37 -0500

    Gulp added to build project

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,22 @@ var path = require("path");
 
 module.exports = {
   entry: {
-    app: ["./app/main.js"]
+    app: ["./src/scripts/main.js"]
   },
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
-    rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
         }
       }
-    }
-  ]
+    ]
   }
 };
