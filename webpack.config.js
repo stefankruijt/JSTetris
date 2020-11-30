b50c723282851998fca951342919c06e0c4b433c
Stefan <stefankruijt1991@gmail.com>
2020-11-29 19:11:25 -0600

    Use webpack instead of gulp

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
-var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: {
-    app: ["./src/scripts/main.js"]
-  },
+  target: "web",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
+  plugins: [
+      new HtmlWebpackPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'static' }
+        ]
+      })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    port: 9000,
+    open: true,
+    watchContentBase: true
   }
 };
