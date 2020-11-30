a55c589d59d109e5e6dbcdbfc43a8bd2dc783231
Stefan <stefankruijt1991@gmail.com>
2020-11-29 19:14:18 -0600

    Create canvas element before game starts
    Don't depend on index.html and canvas.js anymore

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import Game from './modules/game';
+
+console.log("Start Tetris game");
+
+var tetrisGame = document.createElement('canvas');
+tetrisGame.id = 'tetris-game';
+tetrisGame.height = 500;
+tetrisGame.width = 250;
+tetrisGame.style = "background-color:#147479";
+
+document.body.appendChild(tetrisGame); // adds the canvas to the body element
+new Game(tetrisGame);
--- src/scripts/modules/canvas.js
+++
@@ -1,3 +0,0 @@
-export default `
-  <canvas id="tetrisCanvas" width="250" height="500" style="background-color:#147479"></canvas>
-  `
